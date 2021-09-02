7c9aa0e4999a20b166d3b400defb89114c729b25
OZCAP <48883102+OZCAP@users.noreply.github.com>
2021-09-02 13:02:02 +0100

    Update deploy_project.py

--- deploy_project.py
+++ deploy_project.py
@@ -11,9 +11,9 @@ env = input("\nPath to python environment[/myproject/env]: ")
 if env[-1] == '/': env = env[:-1]
 
 # Create user group (if not already made) and add user to that group
-print("\n...\n...\nAdding user {} to group {}...".format(proj, 'webapps'))
-os.system('sudo groupadd --system {}'.format(group))
-stream  = os.popen('sudo useradd --system --gid webapps --shell /bin/bash --home {} {}'.format(root, proj))
+print(f"\n...\n...\nAdding user {proj} to group {group}...")
+os.system(f'sudo groupadd --system {group}')
+stream  = os.popen(f'sudo useradd --system --gid {group} --shell /bin/bash --home {root} {proj}')
 response = stream.read()
 if response == "":
     print("User added successfully!")
@@ -22,7 +22,7 @@ else:
     exit()
 
 print("\nSetting permissions...")
-os.system('sudo chown {} {}'.format(proj, root))
+os.system(f'sudo chown {proj} {root}')
 
 # Create a gunicorn service for the project
 print("\nCreating custom service...")
@@ -31,7 +31,7 @@ try:
         text = f.read()
         service_contents = text.format(group=group, root=root, proj=proj, env=env)
         f.close()
-    with open('/etc/systemd/system/{}.gunicorn.service'.format(proj), 'w') as f:
+    with open(f'/etc/systemd/system/{proj}.gunicorn.service', 'w') as f:
         f.write(service_contents)
         f.close()
         print("Service created! ")
@@ -47,7 +47,7 @@ try:
         socket_contents = text.format(proj=proj, root=root)
         f.close()
 
-    with open('/etc/systemd/system/{}.gunicorn.socket'.format(proj), 'w') as f:
+    with open(f'/etc/systemd/system/{proj}.gunicorn.socket', 'w') as f:
         f.write(socket_contents)
         f.close()
         print("socket file created!")
@@ -57,7 +57,7 @@ except:
 # Start/Enable the socket connection
 print("Enabling service socket...")
 os.system('sudo systemctl daemon-reload')
-stream = os.popen('sudo systemctl start {}.gunicorn.socket'.format(proj))
+stream = os.popen(f'sudo systemctl start {proj}.gunicorn.socket')
 response = stream.read()
 if response == "":
     print("Socket started!")
@@ -65,7 +65,7 @@ else:
     print("Socket start error. Exiting...")
     exit()
 os.system('sudo systemctl daemon-reload')
-stream = os.popen('sudo systemctl enable {}.gunicorn.socket'.format(proj))
+stream = os.popen(f'sudo systemctl enable {proj}.gunicorn.socket')
 response = stream.read()
 if response == "":
     print("Socket enabled!")
@@ -75,11 +75,11 @@ else:
 
 # Reload services
 print("\nReloading daemon...")
-os.system('sudo systemctl daemon-reload && sudo systemctl restart {}.gunicorn'.format(proj))
+os.system(f'sudo systemctl daemon-reload && sudo systemctl restart {proj}.gunicorn')
 
 # Set ownership of the project folder to the specified user
 print("\nSetting permissions...")
-os.system('sudo chown {} {}'.format(proj, root))
+os.system(f'sudo chown {proj} {root}')
 
 # Create the configuration for the website
 print("\nCreating nginx website configuration...")
@@ -88,7 +88,7 @@ try:
         text = f.read()
         config_contents = text.format(domain=domain, root=root, proj=proj)
         f.close()
-    with open('/etc/nginx/sites-available/{}'.format(proj), 'w') as f:
+    with open(f'/etc/nginx/sites-available/{proj}', 'w') as f:
         f.write(config_contents)
         f.close()
         print("Config file created!")
@@ -98,7 +98,7 @@ except:
 
 # Add site to sites-enabled
 print("\nCreating site link...")
-os.system('sudo ln -s /etc/nginx/sites-available/{} /etc/nginx/sites-enabled'.format(proj))
+os.system(f'sudo ln -s /etc/nginx/sites-available/{proj} /etc/nginx/sites-enabled')
 
 # Refresh Nginx
 print("\nRefreshing ngix configuration...")
